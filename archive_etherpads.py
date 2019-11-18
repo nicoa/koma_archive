9d2f778b4768f7b931fcf8cfcb9f31421e55b40e
Nico Albers <nico.albers@aboutyou.de>
2019-11-18 22:00:20 +0100

    seenotrettung hinzugefügt, verschiedene kleinigkeiten

--- archive_etherpads.py
+++ archive_etherpads.py
@@ -6,11 +6,12 @@ Warnings:
     This is python2 (raw_input etc). But, still in mind one might switch at
     some time, so used paths and mostly easy convertible syntax.
 """
-from pathlib2 import PosixPath
-import bs4
 import logging
 import os
+
+import bs4
 import pandas as pd
+from pathlib2 import PosixPath
 import requests
 
 logging.basicConfig(
@@ -58,7 +59,7 @@ def _remove_bad_words(url):
     return url
 
 
-def get_pad_content(url):
+def get_pad_content(url, destination):
     """Read Pad content and write to HTML, after that return found links."""
     if "/p/" not in url:
         logger.info(
@@ -71,10 +72,28 @@ def get_pad_content(url):
         logger.error(e.message)
         return []
 
+    if r.status_code != 200:
+        new_url = "{}/p/{}_seenotrettung".format(
+            "https://fachschaften.rwth-aachen.de/etherpad", url.split("/")[-1])
+        s = "{} -> {}".format(url, new_url)
+        logger.warn("got status code {}, consider moving {}".format(
+            r.status_code, url))
+        if confirm("give Time to save?", resp=True):
+            print s
+            confirm("Done?")
+            return [new_url]
+        else:
+            return []
+    # TODO: Don't hard-code URLs
+
     # create path
     path = PosixPath(
         "/".join(
-            ['pads'] + map(_remove_bad_words, url.split("/p/"))) + '.html')
+            [destination] + map(_remove_bad_words, url.split("/p/"))) + '.txt')
+    html_path = PosixPath(
+        "/".join(
+            [destination] + map(_remove_bad_words, url.split("/p/"))) +
+        '.html')
     if len(path.parts) < 3:
         logger.warn("too few parts in path {}".format(path.as_posix()))
         return []
@@ -87,15 +106,18 @@ def get_pad_content(url):
     else:
         pass
 
-    # write file
-    if not path.exists():
-        with open(path.as_posix(), 'w') as fh:
-            fh.write(r.text.encode('utf-8'))
-    else:
-        logger.debug("path exists already, not saving pad")
-
+    # # write file  # TODO: re-write
+    # if not path.exists():
+    #     with open(path.as_posix(), 'w') as fh:
+    #         fh.write(requests.get(url + '/export/txt').text.encode('utf-8'))
+    # else:
+    #     logger.debug("path exists already, not saving pad")
+    with open(path.as_posix(), 'w') as fh:
+        fh.write(requests.get(url + '/export/txt').text.encode('utf-8'))
+    with open(html_path.as_posix(), 'w') as fh:
+                fh.write(r.text.encode('utf-8'))
     # call other files
-    soup = bs4.BeautifulSoup(r.text)
+    soup = bs4.BeautifulSoup(r.text, features="html.parser")
     links = map(lambda a: a.get('href'), soup.find_all('a'))
     return links
 
@@ -115,13 +137,13 @@ class PadGrabber(object):
         self.base_url = url
         self.urls = []
 
-    def follow_links(self, url):
+    def follow_links(self, url, destination):
         """Main, call get_pad_content recursively on returned links."""
         if url in self.urls:
             logger.info("IGNORE: url already contained")
             return
         self.urls.append(url)
-        links = get_pad_content(url)
+        links = get_pad_content(url, destination)
         for link in links:
             if link:
                 pass
@@ -133,14 +155,14 @@ class PadGrabber(object):
             )
             self.edges.append(verts)
             logger.info('STARTED {}'.format(link.encode('utf-8')))
-            self.follow_links(link)
+            self.follow_links(link, destination)
 
 
 if __name__ == '__main__':
-    pads = PadGrabber(
-        "https://fachschaften.rwth-aachen.de/etherpad/p/KoMaAPSammlung")
-    pads.follow_links(pads.base_url)
+    destination = '../koma-pad-archiv'
+    pads = PadGrabber(os.environ.get("PAD_BASE_URL"))
+    pads.follow_links(pads.base_url, destination)
 
     edges = pd.DataFrame(pads.edges, columns=['from', 'to'])
-    edges.to_csv("pads/edges.csv", encoding='utf-8')
+    edges.to_csv("{}/edges.csv".format(destination), encoding='utf-8')
     logger.info('Successfully wrote edges csv')
