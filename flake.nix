d516d4335e661bd01d78428b590db14415ea46dd
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-12-11 12:51:44 +0100

    Add basic devshell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "utils/flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    utils.lib.mkFlake {
+      inherit self inputs;
+
+      sharedOverlays = [ inputs.poetry2nix.overlays.default ];
+
+      outputsBuilder = channels: {
+        devShells.default = let
+          poetryEnv =
+            channels.nixpkgs.poetry2nix.mkPoetryEnv { projectDir = ./.; };
+        in poetryEnv.env.overrideAttrs
+        (oldAttrs: { buildInputs = [ channels.nixpkgs.poetry ]; });
+      };
+    };
+}
