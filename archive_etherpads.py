e606a7126725527ca123a31a3e2da428ffa85f14
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-12-11 12:56:46 +0100

    Blacken

--- archive_etherpads.py
+++ archive_etherpads.py
@@ -9,10 +9,11 @@ from pathlib2 import PosixPath
 import requests
 
 logging.basicConfig(
-    format='%(asctime)s %(levelname)s:%(message)s',
-    level=logging.INFO, datefmt='%I:%M:%S'
+    format="%(asctime)s %(levelname)s:%(message)s",
+    level=logging.INFO,
+    datefmt="%I:%M:%S",
 )
-logger = logging.getLogger('padgrapper')
+logger = logging.getLogger("padgrapper")
 logger.setLevel(logging.INFO)
 
 
@@ -22,58 +23,63 @@ def confirm(prompt=None, resp=False):
     Returns True for yes and False for no.
     """
     if prompt is None:
-        prompt = 'Confirm'
+        prompt = "Confirm"
 
     if resp:
-        prompt = '%s [%s]|%s: ' % (prompt, 'y', 'n')
+        prompt = "%s [%s]|%s: " % (prompt, "y", "n")
     else:
-        prompt = '%s [%s]|%s: ' % (prompt, 'n', 'y')
+        prompt = "%s [%s]|%s: " % (prompt, "n", "y")
 
     while True:
         ans = input(prompt)
         if not ans:
             return resp
-        if ans not in ['y', 'Y', 'n', 'N']:
-            print('please enter y or n.')
+        if ans not in ["y", "Y", "n", "N"]:
+            print("please enter y or n.")
             continue
-        if ans == 'y' or ans == 'Y':
+        if ans == "y" or ans == "Y":
             return True
-        if ans == 'n' or ans == 'N':
+        if ans == "n" or ans == "N":
             return False
 
 
 def _remove_bad_words(url):
     url = url.replace("/etherpad/p/", "/p/")  # not having two variants
     url = "_".join(
-        x for x in (
-            url.replace("https", "_").replace("http", "_")
-            .replace(":", "_").replace("/", "_").replace(".", "_")
-            .split("_"))
-        if x != "")
+        x
+        for x in (
+            url.replace("https", "_")
+            .replace("http", "_")
+            .replace(":", "_")
+            .replace("/", "_")
+            .replace(".", "_")
+            .split("_")
+        )
+        if x != ""
+    )
     return url
 
 
 def get_pad_content(url, destination):
     """Read Pad content and write to HTML, after that return found links."""
     if "/p/" not in url:
-        logger.info(
-            'IGNORED {} is not a valid pad url'.format(url.encode('utf-8')))
+        logger.info("IGNORED {} is not a valid pad url".format(url.encode("utf-8")))
         return []
 
     try:
-        r = requests.get(url + '/export/html')
+        r = requests.get(url + "/export/html")
     except requests.ConnectionError as e:
         logger.error(e.message)
         return []
 
     if r.status_code != 200:
         new_url = "{}/p/{}_seenotrettung".format(
-            "https://fachschaften.rwth-aachen.de/etherpad", url.split("/")[-1])
+            "https://fachschaften.rwth-aachen.de/etherpad", url.split("/")[-1]
+        )
         s = "{} -> {}".format(url, new_url)
-        logger.warn("got status code {}, consider moving {}".format(
-            r.status_code, url))
+        logger.warn("got status code {}, consider moving {}".format(r.status_code, url))
         if confirm("give Time to save?", resp=True):
-            print s
+            print(s)
             confirm("Done?")
             return [new_url]
         else:
@@ -82,18 +88,19 @@ def get_pad_content(url, destination):
 
     # create path
     path = PosixPath(
-        "/".join(
-            [destination] + list(map(_remove_bad_words, url.split("/p/")))) + '.txt')
+        "/".join([destination] + list(map(_remove_bad_words, url.split("/p/"))))
+        + ".txt"
+    )
     html_path = PosixPath(
-        "/".join(
-            [destination] + list(map(_remove_bad_words, url.split("/p/")))) +
-        '.html')
+        "/".join([destination] + list(map(_remove_bad_words, url.split("/p/"))))
+        + ".html"
+    )
     if len(path.parts) < 3:
         logger.warn("too few parts in path {}".format(path.as_posix()))
         return []
 
     if not path.parent.exists():
-        logger.info('create path {}'.format(path.parent.as_posix()))
+        logger.info("create path {}".format(path.parent.as_posix()))
         if not confirm():
             return []
         os.makedirs(path.parent.as_posix())
@@ -106,13 +113,13 @@ def get_pad_content(url, destination):
     #         fh.write(requests.get(url + '/export/txt').text.encode('utf-8'))
     # else:
     #     logger.debug("path exists already, not saving pad")
-    with open(path.as_posix(), 'w') as fh:
-        fh.write(requests.get(url + '/export/txt').text.encode('utf-8'))
-    with open(html_path.as_posix(), 'w') as fh:
-                fh.write(r.text.encode('utf-8'))
+    with open(path.as_posix(), "w") as fh:
+        fh.write(requests.get(url + "/export/txt").text.encode("utf-8"))
+    with open(html_path.as_posix(), "w") as fh:
+        fh.write(r.text.encode("utf-8"))
     # call other files
     soup = bs4.BeautifulSoup(r.text, features="html.parser")
-    links = [a.get('href') for a in soup.find_all('a')]
+    links = [a.get("href") for a in soup.find_all("a")]
     return links
 
 
@@ -145,18 +152,22 @@ class PadGrabber(object):
                 continue  # catch None
             verts = (
                 "/".join(map(_remove_bad_words, url.split("/p/"))),
-                "/".join(map(_remove_bad_words, link.split("/p/")))
+                "/".join(map(_remove_bad_words, link.split("/p/"))),
             )
             self.edges.append(verts)
-            logger.info('STARTED {}'.format(link.encode('utf-8')))
+            logger.info("STARTED {}".format(link.encode("utf-8")))
             self.follow_links(link, destination)
 
 
-if __name__ == '__main__':
-    destination = '../koma-pad-archiv'
+def main():
+    destination = "../koma-pad-archiv"
     pads = PadGrabber(os.environ.get("PAD_BASE_URL"))
     pads.follow_links(pads.base_url, destination)
 
-    edges = pd.DataFrame(pads.edges, columns=['from', 'to'])
-    edges.to_csv("{}/edges.csv".format(destination), encoding='utf-8')
-    logger.info('Successfully wrote edges csv')
+    edges = pd.DataFrame(pads.edges, columns=["from", "to"])
+    edges.to_csv("{}/edges.csv".format(destination), encoding="utf-8")
+    logger.info("Successfully wrote edges csv")
+
+
+if __name__ == "__main__":
+    main()
